14e1b3b24e1ee06d838d8ffaca258a3c1c7bbc7f
BelenGigante <vinn_a@hotmail.com>
2023-01-19 19:22:11 -0500

    location seeds array created and seed.js coded to populate table. ran and verified table in NoSQL extension

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,15 @@
 const sequelize = require('../config/connection');
+const locationData = require('./location-seeds.json');
+const Location = require('../models/Location');
 
 const init = async () => {
   await sequelize.sync({ force: true });
+
+  await Location.bulkCreate(locationData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   process.exit(0);
 };
 
